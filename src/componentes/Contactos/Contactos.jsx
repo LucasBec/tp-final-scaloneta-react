b2bdca01489d34347d2353ee18a0df15e7a1eedc
LucasBec <89425792+LucasBec@users.noreply.github.com>
2023-08-22 18:54:18 -0300

    se agrego formulario a contactos, falta completar con useEffect y useStatus + dar estilo

--- src/componentes/Contactos/Contactos.jsx
+++ src/componentes/Contactos/Contactos.jsx
@@ -1,7 +1,31 @@
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
 export function Contactos (){
-    return(
-        <div className='container mt-5'>
-            <h1>Contactos</h1>
-        </div>
-    )
+    return (
+    <>
+
+        <h1>Contactos</h1>
+
+        <Form>
+          <fieldset enabled>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Nombre Y Apellido</Form.Label>
+              <Form.Control id="disabledTextInput" type='text' placeholder="" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Correo Electronico</Form.Label>
+              <Form.Control id="disabledTextInput" type='email' placeholder="" />
+            </Form.Group>
+            <Form.Group className="mb-3">
+              <Form.Label htmlFor="disabledTextInput">Mensaje</Form.Label>
+              <Form.Control id="disabledTextInput" type='textarea' placeholder="..." />
+            </Form.Group>
+
+            <Button type="submit">Enviar</Button>
+          </fieldset>
+        </Form>
+        </>
+      );
+      
 }
