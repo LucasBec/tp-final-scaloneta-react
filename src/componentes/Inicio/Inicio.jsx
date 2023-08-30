fc09383a3941645e03ee80bc4a25c0df5439ad25
Kuro <davidsegaespro@gmail.com>
2023-08-30 03:25:30 -0300

    agregue el carrusel de las imagenes a inicio,mande las img a inicio

--- src/componentes/Inicio/Inicio.jsx
+++ src/componentes/Inicio/Inicio.jsx
@@ -1,16 +1,33 @@
-//hook de react
-import { useEffect, useState } from "react";
-// componente personalizado
+import React, { useEffect, useState } from "react";
 import { MiCard } from "./MiCard";
-// clase de bootstrap
-import { Alert } from "react-bootstrap";
+import { Alert, Container, Row, Col } from "react-bootstrap";
+import "./Inicio.css";
+import { Carousel, CarouselItem, CarouselControl, CarouselIndicators, CarouselCaption } from 'reactstrap';
 
-import { Row, Col, Container } from "react-bootstrap"; // Solo importa los componentes que vas a usar
+// Obtén las rutas completas de las imágenes en la carpeta actual
+const imagenHome1 = require('./imagen-home1.jpg');
+const argentinaCampeon = require('./argentina-campeon-mundial-2022.jpg');
+const argentinaLevantandoLaCopa = require('./argentina levantando la copa.jpg');
 
-import "./Inicio.css"; // Importa tu archivo de estilos CSS personalizados si lo tienes
+const items = [
+  {
+    src: imagenHome1,
+    altText: 'Slide 1',
+    caption: 'Slide 1'
+  },
+  {
+    src: argentinaCampeon,
+    altText: 'Slide 2',
+    caption: 'Slide 2'
+  },
+  {
+    src: argentinaLevantandoLaCopa,
+    altText: 'Slide 3',
+    caption: 'Slide 3'
+  }
+];
 
-export function Inicio() {
-
+const Inicio = () => {
   const [datos, setDatos] = useState(null);
 
   useEffect(() => {
@@ -19,10 +36,9 @@ export function Inicio() {
     const consulta = `https://newsapi.org/v2/everything?q=${tema}&sortBy=publishedAt&pageSize=5&language=es&apiKey=${apiKey}`;
 
     fetch(consulta)
-      .then(resp => {
-        resp.json().then(data => {
-          setDatos(data.articles);
-        });
+      .then(resp => resp.json())
+      .then(data => {
+        setDatos(data.articles);
       })
       .catch(error => {
         console.log('error -->', error);
@@ -31,23 +47,16 @@ export function Inicio() {
 
   return (
     <>
-      <div className="parallax-container">
-        <img
-          src="img/imagen-home1.jpg"
-          alt="Portada"
-          className="parallax-image"
-        />
-      </div>
-      
+      <CarouselInicio />
 
       <Container className="p-index-html">
         <Row>
           <Col>
             <p>
-                ¡Bienvenidos a la nueva aplicación de la AFA! En este espacio podrás encontrar todo lo relacionado con la Selección Argentina de fútbol y su cuerpo técnico.
-                Te invitamos a conocer nuestra nueva aplicación Web, desarrollada en colaboración con expertos en tecnología. Esta herramienta única en su tipo permitirá al cuerpo técnico de la Selección Argentina llevar a cabo de manera eficiente la convocatoria de jugadores y la creación del 11 inicial para cada partido.
-                Además, en nuestro sitio encontrarás toda la información actualizada sobre los jugadores, los próximos partidos y el desempeño de la Selección en cada torneo. También podrás acceder a las últimas noticias y novedades del mundo del fútbol argentino.
-                ¡Únete a nuestra comunidad y no te pierdas ningún detalle de la Selección Argentina!
+              ¡Bienvenidos a la nueva aplicación de la AFA! En este espacio podrás encontrar todo lo relacionado con la Selección Argentina de fútbol y su cuerpo técnico.
+              Te invitamos a conocer nuestra nueva aplicación Web, desarrollada en colaboración con expertos en tecnología. Esta herramienta única en su tipo permitirá al cuerpo técnico de la Selección Argentina llevar a cabo de manera eficiente la convocatoria de jugadores y la creación del 11 inicial para cada partido.
+              Además, en nuestro sitio encontrarás toda la información actualizada sobre los jugadores, los próximos partidos y el desempeño de la Selección en cada torneo. También podrás acceder a las últimas noticias y novedades del mundo del fútbol argentino.
+              ¡Únete a nuestra comunidad y no te pierdas ningún detalle de la Selección Argentina!
             </p>
           </Col>
         </Row>
@@ -71,3 +80,75 @@ export function Inicio() {
     </>
   );
 }
+
+class CarouselInicio extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { activeIndex: 0 };
+    this.next = this.next.bind(this);
+    this.previous = this.previous.bind(this);
+    this.goToIndex = this.goToIndex.bind(this);
+    this.onExiting = this.onExiting.bind(this);
+    this.onExited = this.onExited.bind(this);
+  }
+
+  onExiting() {
+    this.animating = true;
+  }
+
+  onExited() {
+    this.animating = false;
+  }
+
+  next() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === items.length - 1 ? 0 : this.state.activeIndex + 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  previous() {
+    if (this.animating) return;
+    const nextIndex = this.state.activeIndex === 0 ? items.length - 1 : this.state.activeIndex - 1;
+    this.setState({ activeIndex: nextIndex });
+  }
+
+  goToIndex(newIndex) {
+    if (this.animating) return;
+    this.setState({ activeIndex: newIndex });
+  }
+
+  render() {
+    const { activeIndex } = this.state;
+
+    const slides = items.map((item) => {
+      return (
+        <CarouselItem
+          onExiting={this.onExiting}
+          onExited={this.onExited}
+          key={item.src}
+        >
+          <div className="parallax-container">
+            <img src={item.src} alt={item.altText} className="parallax-image" />
+          </div>
+          <CarouselCaption captionText={item.caption} captionHeader={item.caption} />
+        </CarouselItem>
+      );
+    });
+
+    return (
+      <Carousel
+        activeIndex={activeIndex}
+        next={this.next}
+        previous={this.previous}
+      >
+        <CarouselIndicators items={items} activeIndex={activeIndex} onClickHandler={this.goToIndex} />
+        {slides}
+        <CarouselControl direction="prev" directionText="Previous" onClickHandler={this.previous} />
+        <CarouselControl direction="next" directionText="Next" onClickHandler={this.next} />
+      </Carousel>
+    );
+  }
+}
+
+export default Inicio;
+export { Inicio, CarouselInicio };
